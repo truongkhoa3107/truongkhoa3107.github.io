4d738c5c0838ec7c436735a4a6cee45c2bfa9de8
TRUONGKHOA <phamtruongkhoa2000@gmail.com>
2021-09-17 16:23:29 +0700

    added new information

---
+++ Creative_theme_bootstrap.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+    window.onbeforeunload = function () {
+        window.scrollTo(0, 0);
+    }
+    $(window).scroll(function () { 
+        // console.log($('.menutop').offset().top);
+        if ($('.menutop').offset().top > 0) {
+            $('.menutop').addClass('biendoi');
+        } else {
+            $('.menutop').removeClass('biendoi');
+        }
+    });
+    $('.logo').click(function(e) {
+        e.preventDefault();
+        $('html, body').animate({scrollTop: 0}, 1000) 
+        $('._1khoi a').parent().removeClass('active');
+    })
+    $('._1khoi a').click(function (e) {
+        e.preventDefault();
+        $('._1khoi a').parent().removeClass('active');
+        $('html, body').animate({scrollTop: $($(this).data('id')).offset().top - 72}, 1000);
+        $(this).parent().addClass('active');
+    })
+});
